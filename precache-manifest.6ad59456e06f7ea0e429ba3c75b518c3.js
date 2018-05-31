d08d1f57537848413fbffb9ca89ea8c029d06b2d
liverwang <live_wang@live.cn>
2018-05-31 09:50:56 +0800

    Add files via upload

---
+++ precache-manifest.6ad59456e06f7ea0e429ba3c75b518c3.js
@@ -0,0 +1,43 @@
+self.__precacheManifest = [
+  {
+    "revision": "21c5a925541b7faea358d7107f9452b8",
+    "url": "./logo-152.png"
+  },
+  {
+    "revision": "fc5ab06feef9eeba24c6f19e558e79f5",
+    "url": "./static/lib/axios.min.js"
+  },
+  {
+    "revision": "5a77e3e12c91e25b47d658712d3d4d05",
+    "url": "./static/lib/flexible.js"
+  },
+  {
+    "url": "./static/js/vendor.ec5d96e9e1239d6efdc4.js"
+  },
+  {
+    "url": "./static/js/manifest.3ad1d5771e9b13dbdad2.js"
+  },
+  {
+    "url": "./static/js/main.e89513c21f334e1f2fbb.js"
+  },
+  {
+    "revision": "0c6fc71ba3344e0e39727d6012e6ff7d",
+    "url": "./static/img/logo.0c6fc71.png"
+  },
+  {
+    "revision": "e89513c21f334e1f2fbb",
+    "url": "./static/css/main.1ffcdc4c8d99e4b1dabe8ba44c8ee82f.css"
+  },
+  {
+    "revision": "b1624a56c41168898d2654e1ce4ff087",
+    "url": "./logo-512.png"
+  },
+  {
+    "revision": "07132e31562a6ee1964bcfdbb36670ad",
+    "url": "./logo-192.png"
+  },
+  {
+    "revision": "99d17cfb90a612258421b38d9be47bdf",
+    "url": "./logo-180.png"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,29 @@
+importScripts("./precache-manifest.bdd77ced4927855395bf82d18f955486.js", "https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
+
+// Whether or not the service worker should skip over the waiting lifecycle stage.
+workbox.skipWaiting()
+// Whether or not the service worker should start controlling any existing clients as soon as it activates
+workbox.clientsClaim()
+
+// change these default names by altering all or some of the values
+// https://developers.google.cn/web/tools/workbox/modules/workbox-core
+workbox.core.setCacheNameDetails({
+  prefix: 'retail'
+})
+
+// A caching strategy is a pattern that determines how a service worker generates a response after receiving a fetch event
+// cachestrategies:https://developers.google.cn/web/tools/workbox/modules/workbox-strategies
+// routing:https://developers.google.cn/web/tools/workbox/modules/workbox-routing
+workbox.routing.registerRoute(
+  /\/sw-register\.js/,
+  workbox.strategies.networkOnly()
+)
+
+workbox.routing.registerRoute(
+  'https://static.mlinks.cc/scripts/dist/mlink.min.js',
+  workbox.strategies.staleWhileRevalidate()
+)
+// determine when and how long a file is cached as well as serve it to the browser without going to the network
+// https://developers.google.cn/web/tools/workbox/modules/workbox-precaching
+workbox.precaching.precacheAndRoute(self.__precacheManifest || [])
+
